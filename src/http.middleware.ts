6e91a30dc0d8a9ca78462e51479868f6c45c8ddd
johnSamilin <temp.kroogi@gmail.com>
2022-11-22 12:24:46 +0300

    Forbids suspicious requests

--- src/http.middleware.ts
+++ src/http.middleware.ts
@@ -1,11 +1,16 @@
 import { HttpStatus, Injectable, NestMiddleware } from '@nestjs/common';
 import { Request, Response } from 'express';
+import { CONFIG } from './config';
 
 @Injectable()
 export class HttpMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: () => void) {
     res.set({ 'Strict-Transport-Security': 'max-age=31536000' });
     console.log(`request ${req.protocol}://${req.hostname}${req.originalUrl}`);
+    if (req.hostname !== CONFIG.HOST) {
+      res.send(HttpStatus.FORBIDDEN);
+      return;
+    }
     if (req.protocol === 'http') {
       res.redirect(
         HttpStatus.PERMANENT_REDIRECT,
