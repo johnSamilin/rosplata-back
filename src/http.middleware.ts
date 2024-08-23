c7d71f50f8245410ddff9eca51b06e7bd590e732
johnSamilin <temp.kroogi@gmail.com>
2024-08-23 13:57:08 +0300

    Fix for nginx

--- src/http.middleware.ts
+++ src/http.middleware.ts
@@ -5,26 +5,28 @@ import { CONFIG } from './config';
 @Injectable()
 export class HttpMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: () => void) {
-    res.set({ 'Strict-Transport-Security': 'max-age=31536000' });
-    // if (!CONFIG.IS_DEV) {
-    //   console.log(
-    //     `request ${req.protocol}://${req.hostname}${req.originalUrl}`,
-    //     req.method,
-    //     req.ip,
-    //     req.headers['user-agent'],
-    //   );
-    // }
+    if (process.env.USE_NGINX !== '1') {
+      res.set({ 'Strict-Transport-Security': 'max-age=31536000' });
+      // if (!CONFIG.IS_DEV) {
+      //   console.log(
+      //     `request ${req.protocol}://${req.hostname}${req.originalUrl}`,
+      //     req.method,
+      //     req.ip,
+      //     req.headers['user-agent'],
+      //   );
+      // }
 
-    if (req.hostname !== CONFIG.HOST) {
-      res.send(HttpStatus.FORBIDDEN);
-      return;
-    }
-    if (req.protocol === 'http' && process.env.USE_NGINX !== '1') {
-      res.redirect(
-        HttpStatus.PERMANENT_REDIRECT,
-        `https://${req.hostname}${req.originalUrl}`,
-      );
-      return;
+      if (req.hostname !== CONFIG.HOST) {
+        res.send(HttpStatus.FORBIDDEN);
+        return;
+      }
+      if (req.protocol === 'http') {
+        res.redirect(
+          HttpStatus.PERMANENT_REDIRECT,
+          `https://${req.hostname}${req.originalUrl}`,
+        );
+        return;
+      }
     }
     next();
   }
